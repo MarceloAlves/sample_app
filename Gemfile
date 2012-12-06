11a3473b584f7f59001830d65de63726cac2ac45
Marcelo Alves <selfmadecelo@gmail.com>
2012-12-05 21:20:51 -0800

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -6,9 +6,7 @@ gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.0.1'
 gem 'will_paginate', '3.0.3'
 gem 'bootstrap-will_paginate', '0.0.6'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
+gem 'jquery-rails', '2.0.2'
 
 group :development, :test do
 	gem 'sqlite3', '1.3.5'
@@ -24,14 +22,8 @@ gem 'annotate', '2.5.0', group: :development
 group :assets do
   gem 'sass-rails',   '3.2.5'
   gem 'coffee-rails', '3.2.2'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '1.2.3'
 end
-
-gem 'jquery-rails', '2.0.2'
 
 group :test do
 	gem 'capybara', '1.1.2'
@@ -45,16 +37,3 @@ end
 group :production do
 	gem 'pg'
 end
-
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'debugger'
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -50,6 +50,9 @@ describe "Authentication" do
 		describe "for non-signed-in users" do
 			let(:user) { FactoryGirl.create(:user) }
 
+			it { should_not have_link('Profile',	href: users_path) }
+			it { should_not have_link('Settings',	href: edit_user_path(user)) }
+
 			describe "when attempting to visit a protected page" do
 				before do
 					visit edit_user_path(user)
@@ -62,20 +65,34 @@ describe "Authentication" do
 					it "should render the desired protected page" do
 						page.should have_selector('title', text: 'Edit user')
 					end
+					
+					describe "when signing in again" do
+						before do
+							delete signout_path
+							visit signin_path
+							fill_in "Email",	with: user.email
+							fill_in "Password",	with: user.password
+							click_button "Sign in"
+						end
+
+						it "should render the default (profile) page" do
+							page.should have_selector('title',	text: user.name)
+						end
+					end
 				end
 			end
 
 			describe "as non-admin user" do
-				let(:user) { FactoryGirl.create(:user) }
-				let(:non_admin) { FactoryGirl.create(:user) }
+		      let(:user) { FactoryGirl.create(:user) }
+		      let(:non_admin) { FactoryGirl.create(:user) }
 
-				before { sign_in non_admin }
+		      before { sign_in non_admin }
 
-				describe "submitting a DELETE request to the Users#destroy action" do
-					before { delete user_path(user) }
-					specify { response.should redirect_to(root_path) }
-				end
-			end
+		      describe "submitting a DELETE request to the Users#destroy action" do
+		        before { delete user_path(user) }
+		        specify { response.should redirect_to(root_path) }
+		      end
+		    end
 
 			describe "in the Users controller" do
 				describe "visiting the edit page" do
@@ -90,6 +107,18 @@ describe "Authentication" do
 
 				describe "submitting to the update action" do
 					before { put user_path(user) }
+					specify { response.should redirect_to(signin_path) }
+				end
+			end
+
+			describe "in the Microposts controller" do
+				describe "submitting to the create actions" do
+					before { post microposts_path }
+					specify { response.should redirect_to(signin_path)}
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete micropost_path(FactoryGirl.create(:micropost)) }
 					specify { response.should redirect_to(signin_path) }
 				end
 			end
